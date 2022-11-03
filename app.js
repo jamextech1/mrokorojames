f6b2d94c6318aacc4664708be11a34b3b4acd8d7
Okoro James <73547857+jamextech1@users.noreply.github.com>
2022-11-03 17:22:07 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector(".hamburger");
+const show = document.querySelector(".nav_links");
+const show2 = document.querySelector(".contact_btn");
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  alert(
+    "Please refresh to remove nav from display, as it's responsive is still in progress"
+  );
+  show.style.display = "flex";
+  show.style.flexDirection = "column";
+  show.style.gap = "2.5rem";
+  show.style.paddingTop = "7.5rem";
+  show.style.position = "absolute";
+  show.style.top = "7rem";
+  show.style.left = "0";
+  show.style.textAlign = "center";
+  show.style.backgroundColor = "rgba(49, 65, 99, 0.39)";
+  show.style.width = "100%";
+  show.style.height = "30rem";
+  show2.style.display = "block";
+});
