6bc63a8a471c3aac080d3839dcda3a609d0a8db1
jamextech1 <okorojameschizaram@gmail.com>
2022-12-12 00:55:35 +0100

    ci/cd

--- app.js
+++ app.js
@@ -48,9 +48,27 @@ openGithub.addEventListener("click", function (e) {
 //
 // End Social Profiles Functions
 
-// scroll to top
-function scrollToTop() {
-  window.scrollTo(0, 0);
+// BACK TO TOP BTN
+// Get the button:
+let mybutton = document.querySelector(".back-to-top-icon");
+
+// When the user scrolls down 80px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 /*==============*/
