fb4bdcc26eb55c04946bffed46332efa5fb15e1e
Okoro James <okorojameschizaram@gmail.com>
2023-06-12 23:24:34 +0100

    new branch initial commit

---
+++ script/app.js
@@ -0,0 +1,134 @@
+const projects = [
+  // project 1
+  {
+    projectImg: `<img src="./images/project-12.jpg"/ class="projectImg">`,
+    projectsTitle: "JNetflix",
+    projectDesc:
+      "JNetflix is a movie website, that you can search and view the information of any movie you like, Built using ReactJs.",
+    projectLink: "https://jnetflixmovie.netlify.app/",
+  },
+  // project 2
+  {
+    projectImg: `<img src="./images/project-14.png"/ class="projectImg">`,
+    projectsTitle: "Fintech",
+    projectDesc: "Fintech Landing single pageg website, built using ReactJs",
+    projectLink: "https://fintechapp.vercel.app/",
+  },
+  // project 3
+  {
+    projectImg: `<img src="./images/project-2.png"/ class="projectImg">`,
+    projectsTitle: "News Landing Page",
+    projectDesc: "News Landing Page built using html,css,and javascript",
+    projectLink: "https://james-news-landing-page-project.netlify.app/",
+  },
+  // project 4
+  {
+    projectImg: `<img src="./images/project-7.png"/ class="projectImg">`,
+    projectsTitle: "Url Shortner",
+    projectDesc: "This website helps you shorten your long url.",
+    projectLink: "https://james-shortlify.netlify.app/",
+  },
+  // project 5
+  {
+    projectImg: `<img src="./images/project-11.jpg"/ class="projectImg">`,
+    projectsTitle: "Search country",
+    projectDesc:
+      "With this website, you can search and locate and as well see the details of any country of your choice",
+    projectLink: "https://dev-get-country.netlify.app/",
+  },
+  // project 6
+  {
+    projectImg: `<img src="./images/project-9.png"/ class="projectImg">`,
+    projectsTitle: "Dictionary Web App",
+    projectDesc:
+      "With this website, you can search and know the meaning of any word.",
+    projectLink: "https://james-dictionary-web.netlify.app/",
+  },
+  // project 7
+  {
+    projectImg: `<img src="./images/project-1.png"/ class="projectImg">`,
+    projectsTitle: "Soft Pinko Website",
+    projectDesc: "Soft Pinko Landing Page Website.",
+    projectLink: "https://james-soft-pinko-project.netlify.app/",
+  },
+  // project 8
+  {
+    projectImg: `<img src="./images/project-4.png"/ class="projectImg">`,
+    projectsTitle: "Easy Banking Website",
+    projectDesc: "This is Easy Banking Landing Page Website.",
+    projectLink: "https://james-easybank-project-1.netlify.app/",
+  },
+  // project 9
+  {
+    projectImg: `<img src="./images/project-5.png"/ class="projectImg">`,
+    projectsTitle: "Tip Calculator",
+    projectDesc: "This website helps you with Tip calculations.",
+    projectLink: "https://james-tip-calc.netlify.app/",
+  },
+  // project 10
+  {
+    projectImg: `<img src="./images/project-6.png"/ class="projectImg">`,
+    projectsTitle: "Github Users",
+    projectDesc:
+      "This website helps you to search and get users info in github, using their usernames.",
+    projectLink: "https://git-users-info-api.netlify.app/",
+  },
+  // project 11
+  {
+    projectImg: `<img src="./images/project-8.png"/ class="projectImg">`,
+    projectsTitle: "News Website",
+    projectDesc:
+      "This website updates you with news of different categories, giving you informations of various happenings in the world.",
+    projectLink: "https://theprimenews.netlify.app/",
+  },
+  // project 12
+  {
+    projectImg: `<img src="./images/project-10.jpg"/ class="projectImg">`,
+    projectsTitle: "Fintekk Landing Page",
+    projectDesc:
+      "This is Fintekk landing page website, diffrent from Fintech landing page webiste.",
+    projectLink: "https://jamex-fintekk-project.netlify.app/",
+  },
+  // project 13
+  {
+    projectImg: `<img src="./images/project-13.jpg"/ class="projectImg">`,
+    projectsTitle: "Gp Website",
+    projectDesc: "This is GP Landing Page Website.",
+    projectLink: "https://gp-bootstrap-project.netlify.app/",
+  },
+  // project 14
+  {
+    projectImg: `<img src="./images/project-3.png"/ class="projectImg">`,
+    projectsTitle: "Bookmark",
+    projectDesc: "This is Bookmark Landing Page Website.",
+    projectLink: "https://james-bookmark-landing-page-project.netlify.app/",
+  },
+];
+//
+//code for handling the displaying projects
+const allProjects = document.querySelector(".projects__second__row");
+console.log(projects);
+projects.map((project) => {
+  allProjects.innerHTML += `
+  
+  <div class="col-9 col-md-4 col-lg-3 shadow project__card">
+  ${project.projectImg}
+  <div class="project__context">
+  <div class="project__title text-danger">${project.projectsTitle}</div>
+  <div class="project__desc text-secondary">${project.projectDesc}</div>
+  <a class="project__link bg-danger" href=${project.projectLink}>Live Preview</a>
+  </div>
+  </div>
+  
+  `;
+});
+//
+/*==============*/
+/*TYPED JS CODE*/
+/*============*/
+let typed = new Typed(".typing-effect", {
+  strings: ["", "Frontend Developer", "Software Developer"],
+  backSpeed: "9",
+  typeSpeed: "9",
+  loop: true,
+});
